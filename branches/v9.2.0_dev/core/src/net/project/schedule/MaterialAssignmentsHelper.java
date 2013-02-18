c6235ef45d11a6a173e90dc7de32fe8f48c9c1e4
nicolas.semeniuk <nicolas.semeniuk@496d5119-df4d-4459-b89f-f6706ac20161>
2013-02-18 18:49:02 +0000

    Cambios en taskview material

---
+++ branches/v9.2.0_dev/core/src/net/project/schedule/MaterialAssignmentsHelper.java
@@ -0,0 +1,19 @@
+package net.project.schedule;
+
+import java.util.ArrayList;
+
+public class MaterialAssignmentsHelper {
+	
+	ArrayList<MaterialAssignmentHelper> materialsAssigned;
+
+	public ArrayList<MaterialAssignmentHelper> getMaterials() {
+		return materialsAssigned;
+	}
+
+	public void setMaterials(ArrayList<MaterialAssignmentHelper> materialsAssigned) {
+		this.materialsAssigned = materialsAssigned;
+	}
+	
+	
+
+}
