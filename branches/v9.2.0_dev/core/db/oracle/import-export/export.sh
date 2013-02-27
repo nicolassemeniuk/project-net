dcf53e34c15c6cf739bad4c584c4a8c5e818fee4
ramiro.savoie <ramiro.savoie@496d5119-df4d-4459-b89f-f6706ac20161>
2013-02-27 19:49:15 +0000

    Mejoras en la presentacion en pantalla de los scripts de importar/exportar

--- branches/v9.2.0_dev/core/db/oracle/import-export/export.sh
+++ branches/v9.2.0_dev/core/db/oracle/import-export/export.sh
@@ -6,22 +6,48 @@
 #: Description		:
 #: Options		:
 
+DOCVAULT='/opt/pnet'
+
+# No es necesario detener la aplicacion, pero deberia hacerse si es existen muchas solicitudes a la base de datos
 # sudo service tomcat6 stop
+
 # Crea una carpeta Backup con la fecha de hoy
 fecha=`date +%d-%m-%y`
 mkdir "Backup ($fecha)" && cd "Backup ($fecha)" || exit 1
 
+# pnet: usuario propietario del esquema
 exp userid=pnet/pnet@XE file=pnet_db_dump.dmp owner=pnet log=pnet_dump.log
+if [ -f pnet_db_dump.dmp ]
+then
+	# Comprimimos los dump y borramos los datos originales
+	tar czf pnet_db_dump.dmp.tar.gz pnet_db_dump.dmp
+	rm pnet_db_dump.dmp
+	printf "\033[32m \n%s \033[0m \n" "Usuario pnet exportado exitosamente"
+else
+	printf "\033[31m \n%s \033[0m \n" "Se ha producido un error al exportar el usuario pnet, revisar los logs"
+	exit
+fi
+
+# pnet_user: usuario que accede a los datos del esquema
 exp userid=pnet_user/pnet_user@XE file=pnet_user_db_dump.dmp owner=pnet_user log=pnet_user_dump.log
+if [ -f pnet_user_db_dump.dmp ]
+then
+	# Comprimimos los dump y borramos los datos originales
+	tar czf pnet_user_db_dump.dmp.tar.gz pnet_user_db_dump.dmp
+	rm pnet_user_db_dump.dmp
+	printf "\033[32m \n%s \033[0m \n" "Usuario pnet_user exportado exitosamente"
+else
+	printf "\033[31m \n%s \033[0m \n" "Se ha producido un error al exportar el usuario pnet_user, revisar los logs"
+	exit
+fi
 
-tar czf pnet_db_dump.dmp.tar.gz pnet_db_dump.dmp
-rm pnet_db_dump.dmp
+printf "\033[32m \n%s \033[0m \n" "Usuarios exportados exitosamente"
 
-tar czf pnet_user_db_dump.dmp.tar.gz pnet_user_db_dump.dmp
-rm pnet_user_db_dump.dmp
+tar czf docVault.tar.gz -C $DOCVAULT docVault
 
-echo 'Backup de la base de datos listo'
-
-tar czf docVault.tar.gz -C /opt/pnet/ docVault
-
-echo 'Backup del document vault listo'
+if [ -f docVault.tar.gz ]
+then
+	printf "\033[32m \n%s \033[0m \n" "Document Vault exportado exitosamente"
+else
+	printf "\033[32m \n%s \033[0m \n" "Se ha producido un error al exportar el Document Vault"
+fi
--- branches/v9.2.0_dev/core/db/oracle/import-export/import.sh
+++ branches/v9.2.0_dev/core/db/oracle/import-export/import.sh
@@ -2,26 +2,43 @@
 #: Title		: Script para importar la bases de datos
 #: Date			: 13/11/2012
 #: Author		: Ramiro Savoie
-#: Version		: 0.1
+#: Version		: 0.5
 #: Description		:
 #: Options		:
+
+# No puede borrarse un usuario si esta conectado
+sudo service tomcat6 stop
 
 # Limpia los datos que ya se encuentren en la base de datos
 sqlplus system/system@XE @clean_and_create_users.sql
 
+# TODO Hacer un chequeo de existencia del archivo y parametrizar la ruta
 tar zxf pnet_db_dump.dmp.tar.gz
 tar zxf pnet_user_db_dump.dmp.tar.gz
+# pnet: usuario propietario del esquema
 imp userid=pnet/pnet@XE file=pnet_db_dump.dmp fromuser=pnet touser=pnet log=pnet_restore.log
+# pnet_user: usuario que accede a los datos del esquema
 imp userid=pnet_user/pnet_user@XE file=pnet_user_db_dump.dmp fromuser=pnet_user touser=pnet_user log=pnet_user_restore.log
 
 # Por si hubo errores al generar algunas vistas
 sqlplus pnet/pnet@XE @compile_views.sql
 
-echo 'Backup de la base de datos listo'
+printf "\033[31m \n%s \033[0m \n" "Backup de la base de datos restaurado"
 
+# TODO Hacer un chequeo de existencia del archivo y parametrizar la ruta
 tar zxf docVault.tar.gz
 sudo cp -R docVault /opt/pnet/
 
-echo 'Backup del document vault listo'
+printf "\033[31m \n%s \033[0m \n" "Backup del document vault restaurado"
 
-sudo service tomcat6 restart
+sudo service tomcat6 start
+
+printf "\033[31m \n%s \033[0m \n" "Durmiendo 1 minuto mientras arranca el Tomcat"
+sleep 1m
+
+# Para que vaya inicializando las caches de Hibernate
+wget --delete-after 'http://localhost:9090/pnet/LoginProcessing.jsp' --user-agent=Mozilla/5.0 --post-data "language=en&theAction=submit&J_USERNAME=appadmin&J_PASSWORD=appadmin&userDomain=1000"
+
+# Limpieza
+rm -rf docVault
+rm -f *dmp
