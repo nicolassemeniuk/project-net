271286102795cc77caa67f577d9a9f0f6c205d75
ramiro.savoie <ramiro.savoie@496d5119-df4d-4459-b89f-f6706ac20161>
2013-04-23 21:56:44 +0000

    Simplificacion de la estructura del XML de materiales y el XSL correspondiente

--- branches/v9.2.0_dev/core/src/net/project/material/PnMaterialList.java
+++ branches/v9.2.0_dev/core/src/net/project/material/PnMaterialList.java
@@ -42,70 +42,18 @@ public class PnMaterialList extends ArrayList<PnMaterial> {
 		while (iterator.hasNext())
 		{
 			PnMaterial material = iterator.next();			
-	        xml.append("<node>");
-        	xml.append("<value>");
-        	xml.append("<isowner>1</isowner>");   
-        
-        	xml.append("<materialspace>"); 
-	        	xml.append("<materialid>" + material.getMaterialId() + "</materialid>");
-	        	xml.append("<materialname>" + material.getMaterialName() + "</materialname>");
-	        	xml.append("<materialdescription>" + material.getMaterialDescription() + "</materialdescription>");
-	        	xml.append("<materialtype>" + material.getPnMaterialType().getMaterialTypeName() + "</materialtype>");
-	        	xml.append("<materialcost>" + material.getMaterialCost() + "</materialcost>");
-	        	xml.append("<materialconsumable>" + material.getMaterialConsumable() + "</materialconsumable>");	        	
-	        	xml.append("<currentdate>" + String.valueOf((new Date()).getTime()) + "</currentdate>");
-        	xml.append("</materialspace>");         
-        
-        	xml.append("</value>");     
-        
-        	xml.append("<children>");
-        	xml.append("</children>");          
-        	xml.append("</node>");   			
-		}
-        xml.append("<maxdepth>1</maxdepth>");		
-		xml.append("</materials-list>\n");
-		return xml.toString();
-	}
-	
-	/**
-	 * Converts the object to XML representation This method returns the object
-	 * as XML text.
-	 * 
-	 * @return XML representation of this object
-	 */
-	public String getXML2() {
-		StringBuffer xml = new StringBuffer();
-		xml.append("<?xml version=\"1.0\" ?>\n\n");
-		xml.append(getXMLBody2());
-		return xml.toString();
-	}
-	
-	/**
-	 * Get an XML representation of the Space without xml tag.
-	 * 
-	 * @return an XML representation of this Space without the xml tag.
-	 */
-	public String getXMLBody2() {
-		Iterator<PnMaterial> iterator = this.iterator();
-		StringBuffer xml = new StringBuffer();
-		xml.append("<materials-list>\n");
-		while (iterator.hasNext())
-		{
-			PnMaterial material = iterator.next();			
-
         
         	xml.append("<material>"); 
-	        	xml.append("<materialid>" + material.getMaterialId() + "</materialid>");
-	        	xml.append("<materialname>" + material.getMaterialName() + "</materialname>");
-	        	xml.append("<materialdescription>" + material.getMaterialDescription() + "</materialdescription>");
-	        	xml.append("<materialtype>" + material.getPnMaterialType().getMaterialTypeName() + "</materialtype>");
-	        	xml.append("<materialcost>" + material.getMaterialCost() + "</materialcost>");	        	
+	        	xml.append("<id>" + material.getMaterialId() + "</id>");
+	        	xml.append("<name>" + material.getMaterialName() + "</name>");
+	        	xml.append("<description>" + material.getMaterialDescription() + "</description>");
+	        	xml.append("<type>" + material.getPnMaterialType().getMaterialTypeName() + "</type>");
+	        	xml.append("<cost>" + material.getMaterialCost() + "</cost>");
+	        	xml.append("<consumable>" + material.getMaterialConsumable() + "</consumable>");	        	
+	        	xml.append("<currentDate>" + String.valueOf((new Date()).getTime()) + "</currentDate>");
         	xml.append("</material>");         
-  			
 		}
-		xml.append("<maxdepth>1</maxdepth>");
 		xml.append("</materials-list>\n");
-
 		return xml.toString();
 	}
 }
