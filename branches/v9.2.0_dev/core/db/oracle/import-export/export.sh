45eb1154779c49099504a566dd09db340170a1c9
ramiro.savoie <ramiro.savoie@496d5119-df4d-4459-b89f-f6706ac20161>
2013-05-09 15:16:11 +0000

    Agregados los prompt para pedir las rutas y migrada la compresion a zip

--- branches/v9.2.0_dev/core/db/oracle/import-export/export.sh
+++ branches/v9.2.0_dev/core/db/oracle/import-export/export.sh
@@ -6,21 +6,20 @@
 #: Description	: Script to export the Project.net database and document vault
 #: Options		:
 
-DOCVAULT='/opt/pnet'
-
 # We don't have to stop the application, but it should be done if there are too many querys on the database.
 # sudo service tomcat6 stop
 
 # Create a Backup folder with today's date
-fecha=`date +%d-%m-%y`
-mkdir "Backup ($fecha)" && cd "Backup ($fecha)" || exit 1
+today=`date +%d-%m-%y`
+mkdir "Backup ($today)" && cd "Backup ($today)" || exit 1
 
 # pnet: owner user of the scheme
 exp userid=pnet/pnet@XE file=pnet_db_dump.dmp owner=pnet log=pnet_dump.log
 if [ -f pnet_db_dump.dmp ]
 then
 	# Compress dump and erase original data
-	tar czf pnet_db_dump.dmp.tar.gz pnet_db_dump.dmp
+	# tar czf pnet_db_dump.dmp.tar.gz pnet_db_dump.dmp
+	zip -r pnet_db_dump.dmp.zip pnet_db_dump.dmp
 	rm pnet_db_dump.dmp
 	printf "\033[32m \n%s \033[0m \n" "Successfully exported user pnet"
 else
@@ -33,7 +32,8 @@ exp userid=pnet_user/pnet_user@XE file=pnet_user_db_dump.dmp owner=pnet_user log
 if [ -f pnet_user_db_dump.dmp ]
 then
 	# Compress dump and erase original data
-	tar czf pnet_user_db_dump.dmp.tar.gz pnet_user_db_dump.dmp
+	# tar czf pnet_user_db_dump.dmp.tar.gz pnet_user_db_dump.dmp
+	zip -r pnet_user_db_dump.dmp.zip pnet_user_db_dump.dmp
 	rm pnet_user_db_dump.dmp
 	printf "\033[32m \n%s \033[0m \n" "Successfully exported user pnet_user"
 else
@@ -43,13 +43,23 @@ fi
 
 printf "\033[32m \n%s \033[0m \n" "Successfully exported all users"
 
-tar czf docVault.tar.gz -C $DOCVAULT docVault
+read -p "Please, insert the path where the Document Vault is (e.g. /opt/pnet) [DEFAULT /opt/pnet]: " DOCVAULT
+if [ -z "$DOCVAULT" ]
+then
+	DOCVAULT='/opt/pnet'
+fi
 
-if [ -f docVault.tar.gz ]
+# tar czf docVault.tar.gz -C $DOCVAULT docVault
+CURRENT=`pwd`
+cd "$DOCVAULT"
+zip -r "$CURRENT/docVault.zip" docVault
+cd "$CURRENT"
+
+if [ -f docVault.zip ]
 then
 	printf "\033[32m \n%s \033[0m \n" "Successfully exported Document Vault"
 else
-	printf "\033[32m \n%s \033[0m \n" "An error occurred when exporting the Document Vault"
+	printf "\033[31m \n%s \033[0m \n" "An error occurred when exporting the Document Vault"
 fi
 
-printf "\033[31m \n%s \033[0m \n" "Export Completed!"
+printf "\033[32m \n%s \033[0m \n" "Export Completed!"
