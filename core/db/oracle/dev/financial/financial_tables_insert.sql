7b19a99e9d5dfc972d9593be7d9ae302b04b9dfd
Ramiro Savoie <rsavoie@users.noreply.github.com>
2013-05-22 20:52:22 +0000

    Agregadas las propiedades del nuevo espacio y del dashboard

--- core/db/oracle/dev/financial/financial_tables_insert.sql
+++ core/db/oracle/dev/financial/financial_tables_insert.sql
@@ -1,7 +1,5 @@
 
-
 select * from PN_PROPERTY a where a.PROPERTY like '%prm.project.propertiesedit.channel.financialstatus.title%';
-
 
 -- Project Create
 insert into PN_PROPERTY values (2000,'en','text','prm.project.create.wizard.financialcalculation.heading','Project Finance Calculation Method','A',0,1,null);
@@ -25,9 +23,27 @@ insert into PN_PROPERTY values (2000,'en','text','prm.project.propertiesedit.com
 insert into PN_PROPERTY values (2000,'en','text','prm.project.propertiesedit.completion.projectcosts.description','Dinamically set the project costs from the costs values.','A',0,1,null);
 insert into PN_PROPERTY values (2000,'en','text','prm.project.propertiesedit.costs.label','Manually insert the project costs values.','A',0,1,null);
 
-
-
-
-
 --Project Meta Properties
 insert into pn_project_space_meta_prop values (8,'CostCalculationMethod', 3);
+
+-- Space definition
+insert into PN_PROPERTY values (2000,'en','text','prm.application.nav.space.financial','Financial','A',0,1,null);
+insert into PN_PROPERTY values (2000,'en','boolean','prm.financial.isenabled','1','A',0,0,null);
+insert into PN_PROPERTY values (2000,'en','text','prm.space.spacetypes.financial.name','Financial','A',0,1,null);
+insert into PN_PROPERTY values (2000,'en','css','prm.global.css.financial','/styles/financial.css','A',0,0,null);
+
+-- Financial portfolio
+insert into PN_PROPERTY values (2000,'en','text','prm.financial.financialportfolio.title','Business List','A',0,1,null);
+insert into PN_PROPERTY values (2000,'en','text','prm.financial.financialportfolio.channel.memberof.title', 'Business finances you have access to','A',0,1,null);
+
+-- Module
+insert into PN_MODULE values (175,'financial_space','@prm.financialspace.module.description', 1, null);
+insert into PN_PROPERTY values (2000,'en','text','prm.finacialsspace.module.description','Financial Workspace','A',0,1,null);
+
+-- Object type
+insert into PN_OBJECT_TYPE values ('financial','pn_financial_space','@prm.global.financial.objecttype.financial',null,1,0,0);
+insert into PN_PROPERTY values (2000,'en','text','prm.global.financial.objecttype.financial','Financial','A',0,1,null);
+
+-- Dashboard
+insert into PN_PROPERTY values (2000,'en','boolean','prm.financial.dashboard.isenabled','1','A',0,0,null);
+insert into PN_PROPERTY values (2000,'en','text','prm.financial.dashboard.title','Dashboard','A',0,1,null);
