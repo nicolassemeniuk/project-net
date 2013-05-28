af436b97b1dc70cd82089635a09945b2a21360b3
Nicolás <nicolas.semeniuk@gmail.com>
2013-05-28 23:16:50 +0000

    Creada la relación entre los negocios y el espacio financiero y entre los espacios financieros asociados a los negocios.

--- core/src/net/project/hibernate/dao/impl/PnSpaceHasSpaceDAOImpl.java
+++ core/src/net/project/hibernate/dao/impl/PnSpaceHasSpaceDAOImpl.java
@@ -17,15 +17,39 @@ package net.project.hibernate.dao.impl;
 import net.project.hibernate.dao.IPnSpaceHasSpaceDAO;
 import net.project.hibernate.model.PnSpaceHasSpace;
 import net.project.hibernate.model.PnSpaceHasSpacePK;
+import net.project.space.SpaceRelationship;
 
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Repository
 public class PnSpaceHasSpaceDAOImpl extends AbstractHibernateAnnotatedDAO<PnSpaceHasSpace, PnSpaceHasSpacePK> implements IPnSpaceHasSpaceDAO {
+
+	private static Logger log = Logger.getLogger(PnSpaceHasSpaceDAOImpl.class);
+
 	public PnSpaceHasSpaceDAOImpl() {
 		super(PnSpaceHasSpace.class);
+	}
+
+	@Override
+	public PnSpaceHasSpace getRelatedFinancialSpace(Integer spaceID) {
+		PnSpaceHasSpace spaceHasSpace = new PnSpaceHasSpace();
+		try {
+			SessionFactory factory = getHibernateTemplate().getSessionFactory();
+			Session session = factory.openSession();
+			spaceHasSpace = (PnSpaceHasSpace) session.createCriteria(PnSpaceHasSpace.class).add(Restrictions.eq("comp_id.parentSpaceId", spaceID))
+					.add(Restrictions.eq("relationshipParentToChild", SpaceRelationship.FINANCIAL.getNameParentToChild())).uniqueResult();
+			session.close();
+		} catch (HibernateException e) {
+			log.error(e.getMessage());
+		}
+		return spaceHasSpace;
 	}
 
 }
