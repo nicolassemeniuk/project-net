c9f2824be7fb170db2bb28acef02db0047137b73
nicolas.semeniuk <nicolas.semeniuk@496d5119-df4d-4459-b89f-f6706ac20161>
2013-06-01 21:33:18 +0000

    Nueva vista de PN_SPACE_VIEW

--- branches/v9.2.0_dev/core/db/oracle/dev/financial/financial_new_tables.sql
+++ branches/v9.2.0_dev/core/db/oracle/dev/financial/financial_new_tables.sql
@@ -236,3 +236,79 @@ END;
 
 
 CREATE OR REPLACE SYNONYM PNET_USER.FINANCIAL FOR PNET.FINANCIAL;
+
+
+CREATE OR REPLACE FORCE VIEW "PNET"."PN_SPACE_VIEW" ("SPACE_ID", "SPACE_NAME", "SPACE_DESC", "SPACE_TYPE", "RECORD_STATUS", "PARENT_SPACE_NAME")
+AS
+  SELECT person_id space_id,
+    display_name space_name ,
+    NULL space_desc ,
+    'person' space_type ,
+    record_status,
+    NULL parent_space_name
+  FROM pn_person
+  UNION
+  SELECT business_id space_id,
+    business_name space_name,
+    business_desc space_desc ,
+    'business' space_type,
+    record_status,
+    NULL parent_space_name
+  FROM pn_business
+  UNION
+  SELECT project_id space_id,
+    project_name space_name,
+    project_desc space_desc ,
+    'project' space_type,
+    record_status,
+    NULL parent_space_name
+  FROM pn_project_space
+  UNION
+  select financial_space_id space_id, financial_space_name space_name, '' space_desc ,  'financial' space_type,  record_status, NULL parent_space_name
+from pn_financial_space
+  UNION
+  SELECT m.methodology_id space_id,
+    m.methodology_name space_name,
+    m.methodology_desc space_desc ,
+    'methodology' space_type,
+    m.record_status,
+    DECODE(b.business_id,NULL,'Personal',b.business_name) parent_space_name
+  FROM pn_methodology_space m,
+    pn_space_has_space s,
+    pn_business b
+  WHERE m.methodology_id = s.child_space_id
+  AND s.parent_space_id  = b.business_id(+)
+  UNION
+  SELECT application_id space_id,
+    application_name space_name,
+    application_desc space_desc ,
+    'application' space_type,
+    record_status,
+    NULL parent_space_name
+  FROM pn_application_space
+  UNION
+  SELECT configuration_id space_id,
+    configuration_name space_name,
+    configuration_desc space_desc ,
+    'configuration' space_type,
+    record_status,
+    NULL parent_space_name
+  FROM pn_configuration_space
+  UNION
+  SELECT 5,
+    '@prm.enterprise.space.type.enterprise.name',
+    '',
+    'enterprise',
+    'A',
+    ''
+  FROM dual
+  UNION
+  SELECT 10,
+    '@prm.resource.space.type.resource.name',
+    '',
+    'resources',
+    'A',
+    ''
+  FROM dual;
+
+CREATE OR REPLACE SYNONYM PNET_USER.PN_SPACE_VIEW FOR PNET.PN_SPACE_VIEW;
