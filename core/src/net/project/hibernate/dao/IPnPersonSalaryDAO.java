309b897e5ebbc02a14d1090ab19fb93be87cb063
Nicolás <nicolas.semeniuk@gmail.com>
2013-06-18 20:44:46 +0000

    Agregados comentarios. Restricción para que el salario actual no tome salarios futuros.

--- core/src/net/project/hibernate/dao/IPnPersonSalaryDAO.java
+++ core/src/net/project/hibernate/dao/IPnPersonSalaryDAO.java
@@ -6,14 +6,27 @@ import net.project.hibernate.model.PnPersonSalary;
 import net.project.hibernate.model.PnPersonSalaryPK;
 
 public interface IPnPersonSalaryDAO extends IDAO<PnPersonSalary, PnPersonSalaryPK> {
-	
+
 	/**
-	 * Obtain a person current salary by the person id.
-	 * @param personID the id of the person we want to obtain the current salary.
+	 * Obtain a person current salary by the person id. This means that the
+	 * salary returned is the one valid for the current date. The future
+	 * salaries in the salary history are not taking in count.
+	 * 
+	 * @param personID
+	 *            the id of the person.
 	 * @return a person salary.
 	 */
 	public PnPersonSalary getCurrentPersonSalaryByPersonId(Integer personID);
-	
+
+	/**
+	 * Obtain a person salary for a certain date.
+	 * 
+	 * @param personId
+	 *            the id of the person.
+	 * @param date
+	 *            the date from which we want to obtain the salary.
+	 * @return the person salary for the period the date is in.
+	 */
 	public PnPersonSalary getPersonSalaryForDate(Integer personId, Date date);
 
 }
--- core/src/net/project/hibernate/service/IPnPersonSalaryService.java
+++ core/src/net/project/hibernate/service/IPnPersonSalaryService.java
@@ -6,17 +6,29 @@ import net.project.hibernate.model.PnPersonSalary;
 import net.project.resource.PersonSalaryBean;
 
 public interface IPnPersonSalaryService {
-	
+
 	public Integer savePersonSalary(PersonSalaryBean personSalary);
-	
+
 	/**
-	 * Obtain the current salary for a person.
-	 * @param personID the id of the person.
+	 * Obtain the current salary for a person. This means that the salary
+	 * returned is the one valid for the current date. The future salaries in
+	 * the salary history are not taking in count.
+	 * 
+	 * @param personID
+	 *            the id of the person.
 	 * @return a person current salary.
 	 */
 	public PnPersonSalary getCurrentPersonSalaryByPersonId(Integer personID);
 
+	/**
+	 * Obtain a person salary for a certain date.
+	 * 
+	 * @param personId
+	 *            the id of the person.
+	 * @param date
+	 *            the date from which we want to obtain the salary.
+	 * @return the person salary for the period the date is in.
+	 */
 	public PnPersonSalary getPersonSalaryForDate(Integer personId, Date date);
-	
 
 }
